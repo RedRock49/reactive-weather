29eff170101d329a7493085c0fb14de8859c4214
RedRock49 <nikitash496@outlook.com>
2023-10-13 19:46:00 +0300

    Updated svgs according to my figma project

--- src/app/page.js
+++ src/app/page.js
@@ -6,23 +6,23 @@ export default function Home() {
     <main >
       <header className='sideBar'>
         <div id="searchField">
-          <button id='searchButton'><Image src="/search.svg" height={35} width={35} alt="Search Icon" /></button>
+          <button id='searchButton'><Image src="/Search.svg" height={35} width={35} alt="Search Icon" /></button>
           <input type='search' id='searchBar' placeholder='Search...'/>
         </div>
         <button id='locationButton'  className='Button' type='button'>
-            <Image src='/location.svg' alt='Location Icon' className='buttonIcon' width={35} height={35} />
+            <Image src='/Location.svg' alt='Location Icon' className='buttonIcon' width={35} height={35} />
             <p className='buttonText'>Location</p>
         </button>
         <ul id='favouriteCityList'>
           <li>
             <button className='favouriteCityButton Button' type='button'>
-              <Image src='./star.svg' alt='Favourite City' className='buttonIcon' width={35} height={35} />
+              <Image src='./Star.svg' alt='Favourite City' className='buttonIcon' width={35} height={35} />
               <p className='buttonText'>Moscow</p>
             </button>
           </li>
         </ul>
         <button id='settingsButton' className='Button'  type='button'>
-            <Image src='/settings.svg' alt='Location Icon' className='buttonIcon' width={35} height={35} />
+            <Image src='/Settings.svg' alt='Location Icon' className='buttonIcon' width={35} height={35} />
             <p className='buttonText'>Settings</p>
         </button>
       </header>
